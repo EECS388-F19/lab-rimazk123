2a4d7f6291487c9db75c56a0847bbe5331548e23
Rimaz M Khan <rimazk@beyster1620p05.engin.umich.edu>
2019-09-09 12:30:43 -0400

    lab 0 completed

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+import random
+
+print("Rimaz")
+a = random.randint(0, 100);
+b = random.randint(0, 100);
+
+print(a)
+print(b)
+
+sum = a + b
+print("Sum = ", sum)
+
+average = (sum)/2
+print("Average = ", average)
